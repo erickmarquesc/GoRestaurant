c6fb01d1a44df3f3a59d7d5c6d0157a706b8876b
erickmarquesc <erickmarques3@gmail.com>
2026-05-24 17:28:32 -0300

    chore: migrate from CRA to Vite

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 });
